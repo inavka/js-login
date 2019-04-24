8f2792f43c7ed06e41387df56385201dbf459614
inavka <ivanka.zhuravchak@gmail.com>
2019-04-24 17:10:46 +0300

    Deleted another task

--- js/main.js
+++ js/main.js
@@ -19,20 +19,5 @@ if (password == null) {
 } 
 }
 
-function getPrimeNumbers() {
-	let firstNumber = prompt('Your first number', 0);
-	let secondNumber = prompt('Your second number', 0);
-
-	let min = (firstNumber <= secondNumber) ? firstNumber : secondNumber;
-	let max = (firstNumber >= secondNumber) ? firstNumber : secondNumber;
-
-	nextPrime:
-	for (var i = min; i < max; i++) {
-		for (var a = 2; a < i; a++) {
-			if (i % a == 0) continue nextPrime;
-				}
-			alert (i);
-			}
-}
 
 	
