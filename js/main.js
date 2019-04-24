f6914f4a6492d0ce6a68239013d91f621945417b
inavka <ivanka.zhuravchak@gmail.com>
2019-04-24 17:07:03 +0300

    Changed variable password: let to var

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let login = prompt('Your login', " ");
 if (login == null) {
     alert('Login canceled');
 } else if (login === "Admin")  {
-    let password = prompt('Your password', "");
+    var password = prompt('Your password', "");
 } else {
     alert('I don\'t know you');
 }
@@ -18,3 +18,21 @@ if (password == null) {
     alert('Password incorrect');
 } 
 }
+
+function getPrimeNumbers() {
+	let firstNumber = prompt('Your first number', 0);
+	let secondNumber = prompt('Your second number', 0);
+
+	let min = (firstNumber <= secondNumber) ? firstNumber : secondNumber;
+	let max = (firstNumber >= secondNumber) ? firstNumber : secondNumber;
+
+	nextPrime:
+	for (var i = min; i < max; i++) {
+		for (var a = 2; a < i; a++) {
+			if (i % a == 0) continue nextPrime;
+				}
+			alert (i);
+			}
+}
+
+	
